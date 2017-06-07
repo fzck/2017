9279d0e572f9707bd1823087237f1db113298e08
fzck <flzauleck@up.edu.ph>
2017-06-07 10:32:01 +0800

    Create DayTemp.cs

---
+++ DayTemp.cs
@@ -0,0 +1,27 @@
+using System;
+
+class DaysTemp
+{
+    public int High, Low; // Declare the instance fields
+    public int Average() // Declare the instance method
+    {
+    return (High + Low) / 2;
+    }
+}
+
+
+public class Test
+{
+	public static void Main()
+	{
+		// your code goes here
+		
+		DaysTemp d1 = new DaysTemp();
+		DaysTemp d2 = new DaysTemp();
+		
+		d1.High = 100;
+		d1.Low = 0;
+		
+		Console.WriteLine("T = {0}, {1}, {2}", d1.Low, d1.High, d1.Average());
+	}
+}
