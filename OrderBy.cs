8180ce21e92ca536c292ca11f78801849c635518
fzck <flzauleck@up.edu.ph>
2017-06-09 16:29:04 +0800

    Create OrderBy.cs

---
+++ OrderBy.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Student
+    {
+        
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new[]
+            {
+              new {Name = "Juan", Address = "Uranus" },
+              new {Name = "Wangwang", Address = "Lalaland"},
+              new {Name = "Maria", Address = "Bethlehem"}
+           };
+
+            var query = from student in students
+                        orderby student.Name
+                        select student;
+
+            foreach (var s in query)
+                Console.WriteLine("Name = {0}, Address = {1}",s.Name,s.Address);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
