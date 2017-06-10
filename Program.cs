76128cce7e2191b806ccc79918a0de45f7ccd313
fzck <flzauleck@up.edu.ph>
2017-06-10 08:58:57 +0800

    square

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareTest.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SquareWidget sq = new SquareWidget();
+            sq.SideLength = 5.0;
+            Console.WriteLine(sq.Area);
+            Console.ReadLine();
+        }
+    }
+}
